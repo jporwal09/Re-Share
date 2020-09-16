046cff41218aa62f8e3ec9851fdd6b3babd11d5d
jporwal09 <jayeshporwal9@gmail.com>
2020-09-16 11:52:21 +0530

    security

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -143,6 +143,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jayesh.prwl@gmail.com'
-EMAIL_HOST_PASSWORD = 'J@yesh50275'
+EMAIL_HOST_PASSWORD = '12345678'
 
 SITE_ID = 1
