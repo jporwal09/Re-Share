e5823c5c568335f1659f8575d0860496b9a6db24
jporwal09 <jayeshporwal9@gmail.com>
2020-09-16 15:37:13 +0530

    Updation of Forgot Password

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jayesh.prwl@gmail.com'
-EMAIL_HOST_PASSWORD = '12345678'
+EMAIL_HOST_USER = 'Jayesh.prwl@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('Password')
 
 SITE_ID = 1
