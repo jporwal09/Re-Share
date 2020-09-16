5c3f41f7eb990e6a656dbbfbf51dd6f1961a1336
jporwal09 <jayeshporwal9@gmail.com>
2020-09-16 21:02:44 +0530

    Additon of profile Page

--- classroom/users/views.py
+++ classroom/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
@@ -20,3 +21,7 @@ def register(request):
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})			
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
